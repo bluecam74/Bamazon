77660adf3467fae56367a028d462bf8d8e03c73a
bluecam74 <mabdulha@uci.edu>
2019-03-24 22:27:49 -0700

    fixes

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,29 +18,19 @@ connection.connect();
  * @param {*} stock_quantity
  */
 
-// function create(flavor, price, stock_quantity) {
-//     connection.query(`
-//         INSERT INTO products (flavor, price, stock_quantity) VALUES (?, ?, ?)
-//         `, [flavor, price, stock_quantity], function (err, res) {
-//             if (err) throw err; 
-//             console.log(`New Flavor: ${flavor} Created!`);
-//             connection.end();
-//         })
-// }
 var productLength;
 
 function read(item_id) {
     if (item_id) {
         connection.query("SELECT * FROM products WHERE item_id = ?", [item_id], function (err, res){
             if (err) throw err; 
-            
-            //console.log(res);
-        })
+            })
     }
     else{
         connection.query("SELECT * FROM products", function (err, res) {
             if (err) throw err; 
             productLength = res.length;
+            console.log("***********************************************************************")
             for (i = 0; i < res.length; i++) { 
                 console.log(`Item ID: ${res[i].item_id} \nProduct Name: ${res[i].product_name} \nDepartment:  ${res[i].department_name} \nPrice: $${res[i].price} \nStock Quantity: ${res[i].stock_quantity}`);
                 console.log("***********************************************************************")
@@ -60,30 +50,27 @@ function update(item_id, stock_quantity, quantity, price) {
         WHERE item_id = ?
         `, [new_stock, item_id], function (err, res) {
         var totalPrice = quantity*price;
-        //console.log(res);
+        console.log("------------------------------------------------------------------");
         console.log("Thank you for your order, your total is $" + totalPrice + "." )
+        console.log("------------------------------------------------------------------");
+        connection.end();
         })
     }
     else {
-        console.log("stock", stock_quantity, "quant", quantity);
+        console.log("-----------------------------------------------------------------------------------------");
         console.log("We do not have enough inventory to fill your order. Please try another item or quantity.")
+        console.log("-----------------------------------------------------------------------------------------");
+        prompt();
     }
     
 }
 
-// function destroy(item_id) {
-//         connection.query("DELETE FROM products WHERE item_id = ?", [id], function (err, res){
-//             if (err) throw err; 
-//             console.log(res);
-//         })
-//     }
 read();
 
 
 function prompt() {
     inquirer   
     .prompt([
-        /* pass questions here*/
         {
         name: "item_id", 
         message: "What is the item ID of the item you would like to purchase?", 
@@ -98,7 +85,6 @@ function prompt() {
     ])
     .then(answers => {
         var item_number = parseInt(answers.item_id);
-        console.log("1");
         var quantity_number = parseInt(answers.quantity);
         if (answers.item_id) {
             connection.query("SELECT * FROM products WHERE item_id = ?", [answers.item_id], function (err, res){
